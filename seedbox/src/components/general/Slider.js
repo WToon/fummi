dd3c53e7256489a18d9398c384b4f6052e222b8a
SerhatErdogan-r0622581 <serhaterdogan2011@hotmail.com>
2019-11-14 00:09:09 +0100

    slider

--- seedbox/src/components/general/Slider.js
+++ seedbox/src/components/general/Slider.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 
 const SliderHeader = styled.p`
+display: flex;
+flex-direction: row;
 padding: 15px;
   font-size: 20px;
-  font-family:'Courier New', Courier, monospace;
+  font-family: Montserrat, sans-serif;
+  color: whitesmoke;
 
 `
 
@@ -22,50 +25,84 @@ const sliderThumbStyles = (props) => (`
 
 `);
 
-const Styles = styled.div`
+const Slidecontainer = styled.div`
+    margin-bottom: 10px;
+    width: 220px;
+`
 
- 
-.slider{
-  flex :6;
-  -webkit-appearance:none;
-  height: 5px;
-  outline: none;
-  margin-bottem: 15px;
-  width: 250px;
+const SliderInput = styled.input`
+-webkit-appearance: none;  /* Override default CSS styles */
+appearance: none;
+width: 200px;
+height: 15px; /* Specified height */
+/*background: rgba(53, 53, 53, 1); !* Grey background *!*/
+background: linear-gradient(90deg, #57ab68 50%, rgba(53, 53, 53, 1) 50%);
+outline: none; /* Remove outline */
+opacity: 1; /* Set transparency to 0.7(for mouse-over effects on hover) */
+-webkit-transition: .2s; /* 0.2 seconds transition on hover */
+transition: opacity .2s;
 
-  &::-webkit-slider-runnable-track {
-    background: black;
-    overflow: hidden;
-    border-radius: 25px;
-    height: auto;
-  }
-  &::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    
-    ${props => sliderThumbStyles(props)}
+border-style: solid;
+border-radius: 25px;
+border-color: rgb(179, 178, 177);
+border-width: 1px;
 
-  }
-  &::-moz-range-thumb{
-    ${props => sliderThumbStyles(props)}
-  }
+&::hover {
+  opacity: 1; /* Fully shown on mouse-over */
+}
+
+&::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  width: 25px; /* Set a specific slider handle width */
+  height: 26px; /* Slider handle height */
+  background: white;
+  cursor: pointer; /* Cursor on hover */
+  border-radius: 30px;
+  margin-top: 1px;
+  /*box-shadow: 2px 2px 1px black;*/
+  border: solid;
+  border-color: #1A1414;
+  border-width: 1px;
+
+}
+
+&::-moz-range-thumb {
+  width: 25px; /* Set a specific slider handle width */
+  height: 25px; /* Slider handle height */
+  background: #b07ac3fc; /* Green background */
+  cursor: pointer; /* Cursor on hover */
+}
+
+&::-webkit-slider-thumb:hover {
+  background: #b07ac3fc; /* Green background */
 }
 `
+
 
 class Slider extends Component {
   state = { value: 50 }
 
+
   render() {
+    const pixelAdjustment = -0.25* this.state.value + 12.5;
+    const percentageAdjustment = pixelAdjustment / 5.76;
+    const pixelValue = parseInt(this.state.value) + percentageAdjustment;
+    const divStyle = {
+      background:'linear-gradient(90deg, rgba(156, 240, 225) ' +  ' ' + pixelValue + '%, rgba(53, 53, 53, 1) ' + pixelValue + '%)'
+    };
     return (
       <>
+        <Slidecontainer >
         <SliderHeader className='slider-header' >{this.props.name} {this.state.value} </SliderHeader>
-        <Styles opacity={this.state.value > 20 ? (this.state.value/100): .2} color = {this.props.color}>
-        <input
+        <SliderInput
+        style={divStyle}
           name={this.props.name}
           className='slider' type='range' min={0} max={100}
           onChange={e => this.setState({ value: e.target.value })}
           onMouseUp={e => this.props.handleOnMouseUp(e)}
         />
-        </Styles>
+        </Slidecontainer>
       </>
     );
   }
